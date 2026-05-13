2f0ffc4e688cab54af3e0f18a0b1c69d37c1ef99
omar <omarkallel93@gmail.com>
2026-05-13 17:08:44 +0100

    Improve admin dashboard with layout, charts, and UI updates

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -7,6 +7,7 @@ use App\Repository\ReservationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/admin')]
 #[IsGranted('ROLE_ADMIN')]
@@ -18,20 +19,15 @@ class AdminController extends AbstractController
         ReservationRepository $reservationRepository
     ): Response {
 
-        // FORCE LOGIN CHECK
-        if (!$this->getUser()) {
-            return $this->redirectToRoute('app_login');
-        }
-
         $carsCount = $carRepository->count([]);
         $reservationsCount = $reservationRepository->count([]);
 
         $reservations = $reservationRepository->findBy([], ['id' => 'DESC'], 5);
 
-        $totalRevenue = 0;
-        foreach ($reservationRepository->findAll() as $r) {
-            $totalRevenue += $r->getTotalPrice();
-        }
+        $totalRevenue = $reservationRepository->createQueryBuilder('r')
+            ->select('SUM(r.totalPrice)')
+            ->getQuery()
+            ->getSingleScalarResult() ?? 0;
 
         return $this->render('admin/dashboard.html.twig', [
             'carsCount' => $carsCount,
